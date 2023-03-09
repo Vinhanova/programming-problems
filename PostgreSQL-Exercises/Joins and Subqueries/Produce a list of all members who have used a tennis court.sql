d9d90a952cff9d439dcf9bfcd2229645b58ff10c
Vinhanova <tiagovinhanova@gmail.com>
2023-03-09 17:03:47 +0000

    Create Produce a list of all members who have used a tennis court.sql

---
+++ PostgreSQL-Exercises/Joins and Subqueries/Produce a list of all members who have used a tennis court.sql
@@ -0,0 +1,42 @@
+/*
+  How can you produce a list of all members who have used a tennis court?
+  Include in your output the name of the court, and the name of the member formatted as a single column.
+  Ensure no duplicate data, and order by the member name followed by the facility name.
+*/
+
+-- Using Concat_ws:
+
+SELECT DISTINCT
+	CONCAT_WS(' ', m.firstname, m.surname) AS member, -- OR --> CONCAT(m.firstname, ' ', m.surname) AS member, 
+	f.name AS facility
+FROM
+	cd.members m,
+	cd.bookings b,
+	cd.facilities f
+WHERE
+	m.memid = b.memid
+	AND
+	b.facid = f.facid
+	AND
+	f.name LIKE 'Tennis%'
+ORDER BY
+	member, facility
+
+
+-- Using String Concatenation Operator:
+
+SELECT DISTINCT
+	m.firstname || ' ' || m.surname AS member,
+	f.name AS facility
+FROM
+	cd.members m,
+	cd.bookings b,
+	cd.facilities f
+WHERE
+	m.memid = b.memid
+	AND
+	b.facid = f.facid
+	AND
+	f.name LIKE 'Tennis%'
+ORDER BY
+	member, facility
