5c68350261a164626b014eb1ce5a4770cce8b50a
Vinhanova <tiagovinhanova@gmail.com>
2023-03-11 16:27:35 +0000

    Create Insert calculated data into a table.sql

---
+++ PostgreSQL-Exercises/3. Modifying data/Insert calculated data into a table.sql
@@ -0,0 +1,31 @@
+/*
+  Let's try adding the spa to the facilities table again.
+  This time, though, we want to automatically generate the value for the next facid, rather than specifying it as a constant.
+  
+  Use the following values for everything else:
+    Name: 'Spa', membercost: 20, guestcost: 30, initialoutlay: 100000, monthlymaintenance: 800.
+*/
+
+
+-- INSERT INTO ... SELECT (recommended):
+
+INSERT INTO
+	cd.facilities (facid, name, membercost, guestcost, initialoutlay, monthlymaintenance)
+SELECT (
+  	SELECT
+		  MAX(facid) + 1
+	  FROM
+	  	cd.facilities
+	),'Spa', 20, 30, 100000, 800
+
+
+-- INSERT INTO ... VALUES (should be used to insert only constant data):
+
+INSERT INTO
+	cd.facilities (facid, name, membercost, guestcost, initialoutlay, monthlymaintenance)
+VALUES ((
+    SELECT
+	  	MAX(facid) + 1
+	  FROM
+	  	cd.facilities
+	),'Spa', 20, 30, 100000, 800)
