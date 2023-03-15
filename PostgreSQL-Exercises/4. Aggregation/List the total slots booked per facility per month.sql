d9918ff09ccbf96d2302a540d3703286c461fb51
Vinhanova <tiagovinhanova@gmail.com>
2023-03-15 19:00:00 +0000

    Create List the total slots booked per facility per month.sql

---
+++ PostgreSQL-Exercises/4. Aggregation/List the total slots booked per facility per month.sql
@@ -0,0 +1,41 @@
+/*
+  Produce a list of the total number of slots booked per facility per month in the year of 2012.
+  Produce an output table consisting of facility id and slots, sorted by the id and month.
+*/
+
+-- First Approach:
+
+SELECT
+	facid,
+	EXTRACT(month FROM starttime) AS month,
+	SUM(slots) AS "Total Slots"
+FROM
+	cd.bookings
+WHERE
+	starttime >= '2012/01/01 00:00:00'
+	AND
+	starttime < '2013/01/01 00:00:00'
+GROUP BY
+	facid,
+	month
+ORDER BY
+	facid,
+	month
+
+
+-- Second Approach:
+
+SELECT
+	facid,
+	EXTRACT(month FROM starttime) AS month,
+	SUM(slots) AS "Total Slots"
+FROM
+	cd.bookings
+WHERE
+	EXTRACT(year FROM starttime) = 2012
+GROUP BY
+	facid,
+	month
+ORDER BY
+	facid,
+	month
