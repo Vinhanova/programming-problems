8d8327abfd13a59313be078943befb883f096516
Vinhanova <tiagovinhanova@gmail.com>
2023-03-16 23:15:00 +0000

    Create List facilities with more than 1000 slots booked.sql

---
+++ PostgreSQL-Exercises/4. Aggregation/List facilities with more than 1000 slots booked.sql
@@ -0,0 +1,16 @@
+/*
+  Produce a list of facilities with more than 1000 slots booked.
+  Produce an output table consisting of facility id and slots, sorted by facility id.
+*/
+
+SELECT
+	facid,
+	SUM(slots) AS "Total Slots"
+FROM
+	cd.bookings
+GROUP BY
+	facid
+HAVING
+	SUM(slots) > 1000
+ORDER BY
+	facid
