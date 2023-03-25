42568801e8952fafa92bc993cf54abb70d959756
Vinhanova <tiagovinhanova@gmail.com>
2023-03-25 10:48:39 +0000

    Create Produce a list of member names, with each row containing the total member count.sql

---
+++ PostgreSQL-Exercises/4. Aggregation/Produce a list of member names, with each row containing the total member count.sql
@@ -0,0 +1,8 @@
+/*
+  Produce a list of member names, with each row containing the total member count.
+  Order by join date, and include guest members.
+*/
+
+SELECT COUNT(*) OVER () AS count, firstname, surname
+  FROM cd.members
+ORDER BY joindate
