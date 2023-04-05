c37bfc88cc7b71c4bd0044e4082b94a540bbfb1f
Vinhanova <tiagovinhanova@gmail.com>
2023-04-05 12:59:12 +0100

    Create Subtract timestamps from each other.sql

---
+++ PostgreSQL-Exercises/5. Working with Timestamps/Subtract timestamps from each other.sql
@@ -0,0 +1,4 @@
+-- Find the result of subtracting the timestamp '2012-07-30 01:00:00' from the timestamp '2012-08-31 01:00:00'
+
+
+SELECT TIMESTAMP '2012-08-31 01:00:00' - TIMESTAMP '2012-07-30 01:00:00' AS INTERVAL
