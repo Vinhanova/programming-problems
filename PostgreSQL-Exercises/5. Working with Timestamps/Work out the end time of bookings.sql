916f997b84d1dd597616d5006dd003bce2f918eb
Vinhanova <tiagovinhanova@gmail.com>
2023-04-09 09:21:11 +0100

    Create Work out the end time of bookings.sql

---
+++ PostgreSQL-Exercises/5. Working with Timestamps/Work out the end time of bookings.sql
@@ -0,0 +1,8 @@
+-- Return a list of the start and end time of the last 10 bookings in the system
+-- (ordered by the time at which they end, followed by the time at which they start)
+
+
+SELECT starttime, starttime + slots * INTERVAL '30 minutes' AS endtime
+	FROM cd.bookings
+ORDER BY endtime DESC, starttime DESC
+LIMIT 10
